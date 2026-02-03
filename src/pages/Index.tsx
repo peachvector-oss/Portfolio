080f04cabd5b228775702c074ff85f7ed1b34bb1
Rothsource <rsovannroth@gmail.com>
2026-02-03 22:28:05 +0700

    Initial commit

---
+++ src/pages/Index.tsx
@@ -0,0 +1,29 @@
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import SkillsSection from "@/components/SkillsSection";
+import ProjectsSection from "@/components/ProjectsSection";
+import AchievementsSection from "@/components/AchievementsSection";
+import CertificatesSection from "@/components/CertificatesSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <AchievementsSection />
+        <CertificatesSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
